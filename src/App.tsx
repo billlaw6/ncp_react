064ffed61feb677bb24979dbbaefd7c3a2138e94
billlaw6 <liubin6@gmail.com>
2019-10-29 22:01:08 +0800

    2019-10-29 22:00 Nested Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +27,9 @@ export default function App() {
                         <li>
                             <Link to="/users">Users</Link>
                         </li>
+                        <li>
+                            <Link to="/topics">Topics</Link>
+                        </li>
                     </ul>
                 </nav>
 
@@ -30,6 +41,9 @@ export default function App() {
                     </Route>
                     <Route path="/users">
                         <Users />
+                    </Route>
+                    <Route path="/topics">
+                        <Topics />
                     </Route>
                     <Route path="/">
                         <Home />
@@ -50,4 +64,47 @@ function About() {
 
 function Users() {
     return <h2>Users</h2>;
+}
+
+function Topics() {
+    let match = useRouteMatch();
+
+    if (match) {
+        return (
+            <div>
+                <h2>Topics</h2>
+
+                <ul>
+                    <li>
+                        <Link to={`${match.url}/components`}>Components</Link>
+                    </li>
+                    <li>
+                        <Link to={`${match.url}/props-v-state`}>
+                            Props v. State
+                        </Link>
+                    </li>
+                </ul>
+
+            {/* The Topics page has its own <Switch> with more routes
+              that build on the /topics URL path. You can think of the
+              2nd <Route> here as an "index" page for all topics, or
+              the page that is shown when no topic is selected */}
+                <Switch>
+                    <Route path={`${match.path}/:topicId`}>
+                        <Topic />
+                    </Route>
+                    <Route path={match.path}>
+                        <h3>Please select a topic.</h3>
+                    </Route>
+                </Switch>
+            </div>
+        );
+    } else {
+        return <h3>Topics</h3>
+    }
+}
+
+function Topic() {
+    let { topicId } = useParams();
+    return <h3>Requested topic ID: {topicId}</h3>;
 }
