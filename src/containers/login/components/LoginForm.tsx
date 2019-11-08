212f921a4c47c6a9d8dfd7368eae9086b92eeab4
billlaw6 <liubin6@gmail.com>
2019-11-08 11:28:37 +0800

    2019-11-08 11:28 combined with antd Form OK

--- src/containers/login/components/LoginForm.tsx
+++ src/containers/login/components/LoginForm.tsx
@@ -1,86 +1,91 @@
 import React from 'react';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-class WrappedNormalLoginForm extends React.Component {
+// 定义IProps，使内部能取到form，解决下面写法的后续报错
+// class NormalLoginForm extends React.Component {
+// Property 'form' does not exist on type 'Readonly<{}> & Readonly<{ children?: ReactNode; }>'.  TS2339
+
+type IProps = Readonly<{ form: any }>
+
+class NormalLoginForm extends React.Component<IProps> {
+    handleSubmit = (e: any) => {
+        e.preventDefault();
+        this.props.form.validateFields((err: any, values: any) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
-        return <div> Login Form </div>;
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item>
+                    {getFieldDecorator('username', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ],
+                    })(
+                        <Input
+                            prefix={
+                                <Icon
+                                    type="user"
+                                    style={{ color: 'rgba(0,0,0,.25)' }}
+                                />
+                            }
+                            placeholder="Username"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ],
+                    })(
+                        <Input
+                            prefix={
+                                <Icon
+                                    type="lock"
+                                    style={{ color: 'rgba(0,0,0,.25)' }}
+                                />
+                            }
+                            type="password"
+                            placeholder="Password"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true,
+                    })(<Checkbox>Remember me</Checkbox>)}
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
+        );
     }
 }
-// const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })((props) => {
-//     handleSubmit = (e: any) => {
-//         e.preventDefault();
-//         console.log(this.props);
-//         this.props.form.validateFields((err, values) => {
-//             if (!err) {
-//                 console.log('Received values of form: ', values);
-//             }
-//         });
-//     };
 
-//     render() {
-//         const { getFieldDecorator } = this.props.form;
-//         return (
-//             <Form onSubmit={this.handleSubmit} className="login-form">
-//                 <Form.Item>
-//                     {getFieldDecorator('username', {
-//                         rules: [
-//                             {
-//                                 required: true,
-//                                 message: 'Please input your username!',
-//                             },
-//                         ],
-//                     })(
-//                         <Input
-//                             prefix={
-//                                 <Icon
-//                                     type="user"
-//                                     style={{ color: 'rgba(0,0,0,.25)' }}
-//                                 />
-//                             }
-//                             placeholder="Username"
-//                         />,
-//                     )}
-//                 </Form.Item>
-//                 <Form.Item>
-//                     {getFieldDecorator('password', {
-//                         rules: [
-//                             {
-//                                 required: true,
-//                                 message: 'Please input your Password!',
-//                             },
-//                         ],
-//                     })(
-//                         <Input
-//                             prefix={
-//                                 <Icon
-//                                     type="lock"
-//                                     style={{ color: 'rgba(0,0,0,.25)' }}
-//                                 />
-//                             }
-//                             type="password"
-//                             placeholder="Password"
-//                         />,
-//                     )}
-//                 </Form.Item>
-//                 <Form.Item>
-//                     {getFieldDecorator('remember', {
-//                         valuePropName: 'checked',
-//                         initialValue: true,
-//                     })(<Checkbox>Remember me</Checkbox>)}
-//                     <a className="login-form-forgot" href="">
-//                         Forgot password
-//                     </a>
-//                     <Button
-//                         type="primary"
-//                         htmlType="submit"
-//                         className="login-form-button">
-//                         Log in
-//                     </Button>
-//                     Or <a href="">register now!</a>
-//                 </Form.Item>
-//             </Form>
-//         );
-//     }
-// });
+
+const WrappedNormalLoginForm = Form.create<IProps>()(
+    NormalLoginForm,
+);
 
 export default WrappedNormalLoginForm;
---
+++ src/containers/login/components/UserManage.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Form, Table, Button, Select, Input, DatePicker } from 'antd';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+const { RangePicker } = DatePicker; //获取日期选择控件中的日期范围控件
+
+class UserManage extends React.Component {
+    render() {
+        const columns = [
+            {
+                title: '联系人',
+                dataIndex: 'userName',
+                key: 'userName',
+            },
+            {
+                title: '手机号',
+                dataIndex: 'mobile',
+                key: 'mobile',
+            },
+            {
+                title: '公司名称',
+                dataIndex: 'companyName',
+                key: 'companyName',
+            },
+            {
+                title: '最近活跃时间',
+                dataIndex: 'lastOnlineTime',
+                key: 'lastOnlineTime',
+            },
+            {
+                title: '禁言状态',
+                dataIndex: 'status',
+                key: 'status',
+            },
+        ];
+
+        return (
+            <div>
+                <Form layout="inline" style={{ marginBottom: '10px' }}>
+                    <FormItem label="最近活跃时间">
+                        <RangePicker style={{ width: '255px' }} />
+                    </FormItem>
+                    <FormItem label="用户">
+                        <Input
+                            type="text"
+                            placeholder="公司名称、手机号"
+                            style={{ width: '155px' }}
+                        />
+                    </FormItem>
+                    <FormItem label="禁言状态">
+                        <Select defaultValue="全部" style={{ width: '155px' }}>
+                            <Option value="全部">全部</Option>
+                            <Option value="是">是</Option>
+                            <Option value="否">否</Option>
+                        </Select>
+                    </FormItem>
+                    <Button
+                        type="primary"
+                        style={{ marginTop: '3px', marginRight: '3px' }}>
+                        查询
+                    </Button>
+                    <Button style={{ marginTop: '3px' }}>重置</Button>
+                </Form>
+                <Table columns={columns} />
+            </div>
+        );
+    }
+}
+
+export default Form.create()(UserManage);
