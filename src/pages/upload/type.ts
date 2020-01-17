cc9e461d9e112d8e92bba7af43d9107e0004dd67
fremango <boping2010@gmail.com>
2020-01-17 10:22:10 +0800

    update Upload page

--- src/pages/upload/type.ts
+++ src/pages/upload/type.ts
@@ -1,13 +1,15 @@
 import { FileProgressStatusEnum } from "_components/FileProgress/type";
 
 export interface UploadStatusI {
-  id: symbol;
+  id: string;
   count: number;
   progress: number;
   status: FileProgressStatusEnum;
+  filePath?: string;
+  failText?: string;
 }
 
-export interface UploadStateI {
-  uploadList: UploadStatusI[];
-  delPrivacy: boolean;
-}
+// export interface UploadStateI {
+//   uploadList: UploadStatusI[];
+//   delPrivacy: boolean;
+// }
