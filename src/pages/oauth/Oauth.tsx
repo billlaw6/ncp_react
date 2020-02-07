1c8342b5709957332d41026546efcc8a7c9ea43e
billlaw6 <liubin6@gmail.com>
2020-02-07 16:18:38 +0800

    2020-02-07 16:18 oauth OK

--- src/pages/oauth/Oauth.tsx
+++ src/pages/oauth/Oauth.tsx
@@ -3,23 +3,26 @@ import { Link } from "react-router-dom";
 import qs from "qs";
 import { connect } from "react-redux";
 import { StoreStateI } from "_constants/interface";
-import { setUserAction } from "_actions/user";
-import { weChatLoginUser } from "_services/user";
-import { replace } from "react-router-redux";
+import { setTokenAction, setUserAction } from "_actions/user";
+import { weChatLoginUser } from "../../services/user";
+import { history } from "../../store/configureStore";
 
 class Oauth extends React.Component<any, any> {
   componentDidMount() {
+    const { setToken, setUser } = this.props;
     const query = this.props.router.location.search.substr(1);
-    console.log(query);
+    // console.log(query);
     const obj = qs.parse(query);
-    console.log(obj);
+    // console.log(obj);
     if (obj.code) {
       // const res = await weChatLoginUser(obj.code);
       // setUserAction(res);
-      weChatLoginUser(obj.code)
+      weChatLoginUser(obj)
         .then(res => {
-          console.log(res);
-          // setUserAction(res.user_info);
+          // console.log(res.data.user_info);
+          setToken(res.data.token);
+          setUser(res.data.user_info);
+          history.replace("/");
         })
         .catch(error => {
           console.log(error);
@@ -46,6 +49,7 @@ const mapStateToProps = (state: StoreStateI) => {
   };
 };
 const mapDispatchToProps = {
-  setUserAction,
+  setToken: setTokenAction,
+  setUser: setUserAction,
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Oauth);
